1714726d8176cfca4cbf288ae5f80832868a3c58
abhishek-p-au16 <abhishekp9991999@gmail.com>
2021-08-10 12:09:32 +0530

    authentication done using jwt

--- backend/routers/user.js
+++ backend/routers/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const userModel = require('../models/User');
+const UserModel = require('../models/User');
 const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
-
+const jwt = require('jsonwebtoken');
+const secretkey = "asdfghjklzxcv"
 
 router.get('/signup',(req, res)=>{
     res.send("signup page")
@@ -20,7 +21,7 @@ router.post('/signup',async(req, res)=>{
         req.body.password = hashedPassword
         console.log(req.body)
 
-        const newUser = new userModel(req.body)
+        const newUser = new UserModel(req.body)
         const saveduser = await newUser.save()
         console.log(saveduser) 
         res.redirect('/')
@@ -34,12 +35,50 @@ router.post('/signup',async(req, res)=>{
 
 
 router.get('/login',(req, res)=>{
-    res.send("loginPage")
+    res.json({success:"loginpage"})
 })
 
 router.post('/login',(req, res)=>{
+    console.log(req.body)
+    UserModel.findOne({email: req.body.email},(err,user)=>{
+        if(!user){
+            return res.status(401).json({ error: 'User not found'})
+        }
+
+        validpass = bcrypt.compare(req.body.password,user.password)
+        if(!validpass){ return res.status(500).json({ error: 'invalid password'})}
+        console.log(user);
+        //res.redirect('/')
+        // usero={username:user.username,
+        //     password:user.password}
+        // const token  = generatejwt(usero)
+        // console.log(token);
+        // res.status(200).send({auth:true,token:token})
+       
+        let token = jwt.sign({id:user}, secretkey, {expiresIn: 500000})
+            return res.status(200).send({auth : true, token: token})
+    })
+
     
 
 })
+
+router.get('/cart',authenticateToken,(req, res)=>{
+    res.send({products:"cart products"})
+})
+// const secretkey = "qwerdfghbnm"
+// function generatejwt(payload) {
+    
+//     return jwt.sign(payload,secretkey,{expiresIn:'60s'})
+// }
+function authenticateToken(req,res,next){
+    const authToken = req.headers.authentication
+    console.log(authToken)
+    if(!authToken){ return res.json({error:true})}
+    const veryfied = jwt.verify(authToken,secretkey)
+    console.log(veryfied)
+    next()
+ }
+
 
 module.exports =router
