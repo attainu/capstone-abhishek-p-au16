b62149cdbd92b17dee4d563995e5b78b411a1835
Ankur Sonkar <ankur.hraa@gmail.com>
2021-08-25 09:58:00 +0530

    final commit

---
+++ frontend/src/data.js
@@ -0,0 +1,36 @@
+export default {
+  products: [
+    {
+      _id: "1",
+      name: "Slim Shirt",
+      category: "Shirt",
+      image: "",
+      price: 50,
+      brand: "Nike",
+      rating: 4.3,
+      numReviews: 10,
+    },
+
+    {
+      _id: "2",
+      name: "Fit Shirt",
+      category: "Shirt",
+      image: "",
+      price: 80,
+      brand: "Nike",
+      rating: 4.6,
+      numReviews: 10,
+    },
+
+    {
+      _id: "3",
+      name: "Slim Shirt",
+      category: "Pants",
+      image: "",
+      price: 60,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+    },
+  ],
+};
